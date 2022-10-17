12332f0e0d0a2991de0635af072ed6be423fa13d
Mesut Özkan <Ozkaanmesuud@gmail.com>
2022-10-17 22:56:56 +0300

    abstractDemo

---
+++ CampWeek_03/homeWork_01/abstractDemo/src/SqlServerDatabaseManager.java
@@ -0,0 +1,7 @@
+public class SqlServerDatabaseManager extends BaseDatabaseManager{
+
+    @Override
+    public void getData() {
+        System.out.println("Veri gönderildi : Sql Server Database");
+    }
+}
